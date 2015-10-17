470b023d9070839838009454536e1df6b6bb7260
wayou <liuwayong@gmail.com>
2015-10-17 17:50:02 +0800

    Add WeChat.app v6.0.0

---
+++ Casks/wechat.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'wechat' do
+  version :latest
+  sha256 :no_check
+  url 'http://dldir1.qq.com/foxmail/Mac/WeChat-en_US.dmg'
+  name 'WeChat'
+  homepage 'http://weixin.qq.com/cgi-bin/readtemplate?t=mac&lang=en'
+  license :commercial
+  app 'WeChat.app'
+  uninstall :quit => 'com.tencent.xinWeChat'
+end
